57cd596a113f233380f9bccd268d0ecf97aa8227
tagreene <greenetrentaustin@gmail.com>
2017-10-11 02:31:46 -0500

    Core Data integration basically done

--- Daily/AnalyticsViewController.swift
+++ Daily/AnalyticsViewController.swift
@@ -30,7 +30,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     var endDateDate: Date!
     var firstEntryDate: Date!
     var lastEntryDate: Date!
-    
+    var entries = [Entry]()
     
     var ourGreen = UIColor.init(red: 95.0/255.0, green: 255.0/255.0, blue: 88.0/255.0, alpha: 0.8)
     var ourYellow = UIColor.init(red: 255.0/255.0, green: 255.0/255.0, blue: 89.0/255.0, alpha: 0.9)
@@ -41,10 +41,10 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     let pieChartDataLabels = ["Completed", "Missed"]
     let pieChartCompletionData = [0.36, 0.64]
     
-//    let barChartDataLabels = [Date.init(timeInterval: -604800, since: Date()), Date.init(timeInterval: -518400, since: Date()), Date.init(timeInterval: -345600, since: Date()), Date.init(timeInterval: -259200, since: Date()), Date.init(timeInterval: -172800, since: Date()), Date.init(timeInterval: -86400, since: Date()), Date()]
-//    let barChartEntryCountData = [1, 1, 2, 28, 1, 2, 1]
+    //    let barChartDataLabels = [Date.init(timeInterval: -604800, since: Date()), Date.init(timeInterval: -518400, since: Date()), Date.init(timeInterval: -345600, since: Date()), Date.init(timeInterval: -259200, since: Date()), Date.init(timeInterval: -172800, since: Date()), Date.init(timeInterval: -86400, since: Date()), Date()]
+    //    let barChartEntryCountData = [1, 1, 2, 28, 1, 2, 1]
     
-//    Super large data test set
+    //    Super large data test set
     let barChartDataLabels = [Date.init(timeInterval: -604800, since: Date()), Date.init(timeInterval: -518400, since: Date()), Date.init(timeInterval: -345600, since: Date()), Date.init(timeInterval: -259200, since: Date()), Date.init(timeInterval: -172800, since: Date()), Date.init(timeInterval: -86400, since: Date()), Date(), Date.init(timeInterval: -604800, since: Date()), Date.init(timeInterval: -518400, since: Date()), Date.init(timeInterval: -345600, since: Date()), Date.init(timeInterval: -259200, since: Date()), Date.init(timeInterval: -172800, since: Date()), Date.init(timeInterval: -86400, since: Date()), Date(), Date.init(timeInterval: -604800, since: Date()), Date.init(timeInterval: -518400, since: Date()), Date.init(timeInterval: -345600, since: Date()), Date.init(timeInterval: -259200, since: Date()), Date.init(timeInterval: -172800, since: Date()), Date.init(timeInterval: -86400, since: Date()), Date()]
     let barChartEntryCountData = [1, 2, 1, 1, 1, 2, 5, 1, 2, 1, 1, 1, 2, 0, 1, 2, 1, 1, 1, 2, 5]
     
@@ -55,29 +55,42 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // set up charts here
-//        view.backgroundColor = .white
-        setUpDates()
+        //        view.backgroundColor = .white
+        queryForMinMaxDates()
         setUpDateSelection()
         setUpBarChart(dates: barChartDataLabels, entryCount: barChartEntryCountData, color: ourBlue)
         setUpPieChart(labels: pieChartDataLabels, values: pieChartCompletionData)
         setUpWordLabels()
         initTapGestureRecognizer()
-
+        
         addGradient()
     }
     
-    func setUpDates() {
+    override func viewWillAppear(_ animated: Bool) {
+        updateMinMaxDateQuery()
+        updateEntities()
+    }
+    
+    func updateMinMaxDateQuery() {
+        queryForMinMaxDates()
+        startDatePicker.minimumDate = firstEntryDate
+        startDatePicker.maximumDate = lastEntryDate
+        endDatePicker.minimumDate = firstEntryDate
+        endDatePicker.maximumDate = lastEntryDate
+    }
+    
+    func queryForMinMaxDates() {
         let context = AppDelegate.viewContext
         
         /*
          Currently, if you use perform here, your app will crash as setUpDateSelection() will try to unwrap firstEntryDate
          before you've retrieved them
- 
+         
          i.e. you have a race condition
- 
+         
          performAndWait solves this, but maybe theres a better way
          perhaps pass set up dates in a closure?
- 
+         
          As of now, performAndWait does not cause a real performance loss, so maybe that is the best answer
          */
         
@@ -149,7 +162,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         
         averageWordLengthLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -16).isActive = true
         averageWordLengthLabel.topAnchor.constraint(equalTo: averageWordLengthDescription.bottomAnchor, constant: 0).isActive = true
-
+        
         averageWordLengthDescription.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -16).isActive = true
     }
     
@@ -168,8 +181,8 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         
         // The below mimics setting the spaceBottom to zero, but also guards against the x axis autogenerating its minimum to a >0 value in highly dynamic sets
         activityChart.leftAxis.axisMinimum = 0.0
-
-    
+        
+        
         
         
         
@@ -216,7 +229,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         pieChartDataSet.setColors(ourYellow, ourGreen)
         pieChartDataSet.drawValuesEnabled = false
         let pieChartData = PieChartData(dataSet: pieChartDataSet)
-
+        
         self.completionChart.data = pieChartData
         completionChart.translatesAutoresizingMaskIntoConstraints = false
         completionChart.legend.enabled = false
@@ -233,7 +246,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         completionChart.widthAnchor.constraint(equalToConstant: screenWidth * 0.5).isActive = true
         completionChart.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         completionChart.bottomAnchor.constraint(equalTo: activityChartLabel.topAnchor, constant: -8).isActive = true
-
+        
         pieChartLabel = UILabel()
         let ourPercent = Int(round(pieChartCompletionData[1] * 100))
         pieChartLabel.text = "\(ourPercent)% of days contained entries in this period"
@@ -262,13 +275,14 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     
     func setUpDateSelection() {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/YY"
+        dateFormatter.dateFormat = "MM/dd/yy"
         
         startDatePicker = UIDatePicker()
         startDatePicker.addTarget(self, action: #selector(handleStartDatePicker(_:)), for: .valueChanged)
         startDatePicker.datePickerMode = .date
         startDatePicker.minimumDate = firstEntryDate
         startDatePicker.maximumDate = lastEntryDate
+        startDatePicker.setDate(firstEntryDate, animated: false)
         startDate = UITextField()
         startDate.inputView = startDatePicker
         startDate.text = dateFormatter.string(from: firstEntryDate)
@@ -297,6 +311,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         submitButton.titleLabel!.font = .systemFont(ofSize: 24)
         submitButton.backgroundColor = ourGreen
         submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.addTarget(self, action: #selector(submitButtonAction(_:)), for: .touchUpInside)
         view.addSubview(submitButton)
         
         let margins = view.layoutMarginsGuide
@@ -321,6 +336,45 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
         self.activeTextField = textField
     }
     
+    @objc func submitButtonAction(_ sender: UIButton) {
+        updateEntities()
+    }
+    
+    func updateEntities() {
+        guard let startDateString = startDate.text, let endDateString = endDate.text else {
+            return
+        }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy"
+        
+        let firstDate = dateFormatter.date(from: startDateString)
+        var lastDate = dateFormatter.date(from: endDateString)
+        guard let unwrappedLastDate = lastDate else { return }
+        lastDate = Date(timeInterval: 60 * 60 * 24, since: unwrappedLastDate)
+        
+        print("\(firstDate), \(lastDate)")
+        
+        let context = AppDelegate.viewContext
+        let ourRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        ourRequest.sortDescriptors = []
+        ourRequest.predicate = NSPredicate(format: "date >= %a AND date < %a", argumentArray: [firstDate as! NSDate, lastDate as! NSDate])
+        
+        context.perform {
+            let entryArray = try? context.fetch(ourRequest)
+            
+            guard let ourEntryArray = entryArray else {
+                print("No Entries???")
+                return
+            }
+            
+            for entry in ourEntryArray {
+                print(entry.date, entry.text)
+            }
+            
+            self.entries = entryArray!
+        }
+    }
+    
     func initTapGestureRecognizer() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
         
@@ -329,7 +383,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     
     @objc func handleStartDatePicker(_ sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/YY"
+        dateFormatter.dateFormat = "MM/dd/yy"
         startDate.text = dateFormatter.string(from: sender.date)
         startDateDate = sender.date
         print(startDateDate)
@@ -337,7 +391,7 @@ class AnalyticsViewController: UIViewController, UITextFieldDelegate {
     
     @objc func handleEndDatePicker(_ sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/YY"
+        dateFormatter.dateFormat = "MM/dd/yy"
         endDate.text = dateFormatter.string(from: sender.date)
         endDateDate = sender.date
         print(endDateDate)
